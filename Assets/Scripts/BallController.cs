5d916ded0e5ca0c35f80985bde46a77062e94702
Azim Momin <amomin@zynga.com>
2019-07-30 10:29:13 -0700

    Adding launch velocity to ball on mouse click.

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,30 +4,41 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] private PaddleController paddleController = null;
     [SerializeField] private Rigidbody2D ballRigidBody = null;
-    [SerializeField] private float launchSpeed = 10.0F;
+    [SerializeField] private Vector2 ballVelocityOnLaunch = Vector2.zero;
 
     private Vector2 distanceBetweenBallAndPaddle;
     private bool hasBallBeenLaunched = false;
     private void Start()
     {
         distanceBetweenBallAndPaddle = transform.position - paddleController.transform.position;
+        if (ballRigidBody == null)
+        {
+            ballRigidBody = GetComponent<Rigidbody2D>();
+        }
     }
 
     private void Update()
     {
-
-        Vector2 currentPaddlePosition = new Vector2(paddleController.transform.position.x,
-   paddleController.transform.position.y);
-        transform.position = currentPaddlePosition + distanceBetweenBallAndPaddle;
-
-        /*
         if (hasBallBeenLaunched == false)
         {
-            if (Input.GetMouseButtonUp(0))
+            LockBallToPaddle();
+            if (Input.GetMouseButtonDown(0)) // Left Mouse Button Click.
             {
-                ballRigidBody.AddForce(new Vector2(0.0F, launchSpeed));
+                LaunchBall();
             }
         }
-        */
+    }
+
+    private void LockBallToPaddle()
+    {
+        // TODO: Disable physics simulator while you are moving the ball with the paddle.
+        Vector2 currentPaddlePosition = new Vector2(paddleController.transform.position.x, paddleController.transform.position.y);
+        transform.position = currentPaddlePosition + distanceBetweenBallAndPaddle;
+    }
+
+    private void LaunchBall()
+    {
+        ballRigidBody.velocity = ballVelocityOnLaunch;
+        hasBallBeenLaunched = true;
     }
 }
