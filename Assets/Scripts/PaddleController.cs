8fede6b232dc963cfd375682690379d868d6c942
Azim Momin <amomin@zynga.com>
2019-07-30 11:48:52 -0700

    Replacing paddle with polygon

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -2,11 +2,16 @@
 
 public class PaddleController : MonoBehaviour
 {
+    private PolygonCollider2D paddleCollider = null;
+    private void Start()
+    {
+        paddleCollider = GetComponent<PolygonCollider2D>();
+    }
     private void Update()
     {
         float cameraHorizontalSize = 2 * Camera.main.orthographicSize * Camera.main.aspect;
         float paddleX = (Input.mousePosition.x / Screen.width) * cameraHorizontalSize;
-        Vector3 paddleBounds = GetComponent<CapsuleCollider2D>().bounds.size;
+        Vector3 paddleBounds = paddleCollider.bounds.size;
         paddleX = Mathf.Clamp(paddleX, paddleBounds.x / 2.0F, cameraHorizontalSize - (paddleBounds.x / 2.0F));
 
         Vector3 currentPaddleTransform = transform.position;
